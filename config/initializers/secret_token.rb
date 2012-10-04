35a3be335537eebcf05a481598d17587c5076169
Vibhor Mahajan <vibhor.mahajan@gmail.com>
2012-10-04 21:30:59 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dealtraction::Application.config.secret_token = 'a333264923eac6ad067d0acd017a0813b108568b0a31f2561047f0250e89a7b13980a84e3048db1cc6ab4a3b93ab535de62ca1abe9c8db4e4dac85b40618471f'
