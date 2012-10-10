4a59f1541706058b5011c68eb519a275663cb733
Aastha <aastha.tangri@trantorinc.com>
2012-10-10 16:57:58 +0530

    main page features

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,3 +26,43 @@ $('#company_hide_info').click(function(){
     $('#company_hide_info').hide();
     $('#company_detail').slideToggle();
 });
+
+function show_reminder(id){ 
+    function myContent(){      
+        t2= $("#news_"+id).text();
+        return "<div style='color: #1B75BB'>"+t2+"</div><textarea rows='4' cols='40' placeholder='Add a note to Reminders...'></textarea><button type='button' style='right:7px;bottom:7px;position:absolute'>Add</button>"
+    };
+    function myTitle(){
+        t1= $("#news_header_"+id).text();
+        btn = "<button  class='close' 'id='close_popover' onclick=\"hide_popover('"+id+"')\" style='position:absolute;top:3px;right:5px'>×</button>"
+        return  t1+btn;
+    }  
+    $('#'+id).popover({
+        html:true, 
+        content:myContent(), 
+        animation:true, 
+        placement: 'left',
+        title: myTitle()        
+    })
+};
+
+function hide_popover(id) {
+//    alert(id);
+   $('#'+id).popover('hide');
+};
+
+function share_news_alert(company_name,id){
+    function myContent(){ 
+        return "<ul style='list-style:none;margin-left:0px'><li style='vertical-align: bottom;line-height:35px;border-bottom: 1px solid lightgray;'><img src='/assets/linkedin.png' height = '26px' width = '26px'><span style = 'padding-left: 20px'>Linkedin</span></li><li  style='vertical-align: bottom;line-height:35px;border-bottom: 1px solid lightgray;'><img src='/assets/fb.png' height = '26px' width = '26px'><span style = 'padding-left: 20px'>Facebook</span></li><li style='vertical-align: bottom;line-height:35px;border-bottom: 1px solid lightgray;'><img src='/assets/google.png' height = '26px' width = '26px'><span style = 'padding-left: 20px'>Google+</span></li><li style='vertical-align: bottom;line-height:35px;border-bottom: 1px solid lightgray;'><img src='/assets/twitter.png' height = '26px' width = '26px'><span style = 'padding-left: 20px'>Twitter</span></li><li style='vertical-align: bottom;line-height:35px;border-bottom: 1px solid lightgray;'><img src='/assets/Black_Email.png' height = '26px' width = '26px'><span style = 'padding-left: 20px'>Email</span></li></ul> "
+    };
+    
+    $('#'+id).popover({
+        html:true, 
+        content:myContent(), 
+        animation:true, 
+        placement: 'left',
+        title: "<span style = 'color: #8F9093'>Share recent news about <b>"+company_name+"</b></span>"
+    })  
+}
+
+
