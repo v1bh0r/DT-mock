8a11551afa83491958b484ee51cc2d5c5391c5d8
Aastha <aastha.tangri@trantorinc.com>
2012-10-12 17:04:34 +0530

    routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Dealtraction::Application.routes.draw do
   get "home/index"
-
+  match "home/reminder" => "home#reminder"
+  match "home/filter" => "home#filter"
+  match "landing" => "home#landing_page"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +52,7 @@ Dealtraction::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
